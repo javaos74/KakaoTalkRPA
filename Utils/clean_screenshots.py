4b817f5cd73b31f0bb6712d4cb638c54ff6a9596
Hyungsoo Kim <javaos74@gmail.com>
2019-01-13 19:54:01 +0900

    update on clean screenshots folder

--- Utils/clean_screenshots.py
+++ Utils/clean_screenshots.py
@@ -12,30 +12,32 @@ def get_all_screeninformation( root):
     return sceenshots
 
 
-
 def mark_used_screenshots( cur_dir, valid_sreenshot_list):
     files = os.listdir( cur_dir)
     for f in files:
         if not f.startswith('.') and os.path.isdir( f) :
-            print('call mark_used_screenshots with %s' %(cur_dir+os.sep+f))
+            print('call mark_used_screenshots with folder %s' %(f))
             mark_used_screenshots( cur_dir + os.sep + f, valid_sreenshot_list) # recursive call 
         if '.xaml' in f: # xaml file 
             root = ET.parse( cur_dir + os.sep + f).getroot() # root element 
             screenshots = get_all_screeninformation(root)
             valid_screenshot_list.extend( screenshots)
-    #print(valid_screenshot_list)
 
-def print_and_remove_unused_screenshots( cur_dir, valid_sreenshot_list):
-    sshots = os.listdir('.screenshots')
+def print_and_remove_unused_screenshots( cur_dir, valid_sreenshot_list, remove=False):
+    sshots = os.listdir( cur_dir + os.sep + '.screenshots')
     print("valid screenshot # %d and .screenshots has # %d" %(len(valid_sreenshot_list), len(sshots)-2))
     for f in sshots:
-        if not f[0: len(f)-4] in valid_sreenshot_list:
+        if ".png" in f and not f[0: len(f)-4] in valid_sreenshot_list:
+            unused_f = '{0}{1}.screenshots{1}{2}'.format( cur_dir, os.sep, f) 
             print('found unused screenshots : %s' %( f))
-            os.system('git rm .screenshots%s%s' %( os.sep, f))
+            #os.system('git rm .screenshots%s%s' %( os.sep, f))
+            if remove and os.path.isfile( unused_f) and '.png' in f :
+                #os.remove( unused_f)
+                print('forced to remove %s' %(unused_f))
 
 if __name__ == '__main__':
     cur_dir = os.getcwd()
     if len(sys.argv) == 2 :
         cur_dir = cur_dir + os.sep + sys.argv[1]
     mark_used_screenshots( cur_dir, valid_screenshot_list)
-    print_and_remove_unused_screenshots( cur_dir, valid_screenshot_list)
+    print_and_remove_unused_screenshots( cur_dir, valid_screenshot_list, True)
